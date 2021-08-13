e8165fcf985ca96cbaa856fb5a763a0ae38579c8
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-12 21:56:06 -0500

    Npm Install && Gallery List & Gallery item created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  let [galleryList, setGalleryList] = useState([])
+
+  useEffect(() =>{
+    //Similar to onReady running doe on load
+    FetchGallery();
+
+  })
+
+  const FetchGallery = () => {
+    //when declaring a FetchGallery that will be responsible for 
+    //fetching gallery from the servers
+    axios.get('/gallery').then((response) => {
+      //Axios.get is requesting data from the server which becomes the response.data
+      console.log('this is the response from fetchGallery', response.data);
+      //Logging data to test the response data
+      setGalleryList(response.data)
+      // take the data from the response and assign it to the setGalleryList
+    }).catch(error => {
+      console.log('GET /Gallery', error);
+    });
+  }
+
+
     return (
       <div className="App">
         <header className="App-header">
